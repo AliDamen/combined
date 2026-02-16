4e124e27b4c8f79171051c069d5e650fd796a4ab
AliDamen <ali.damen69.md@gmail.com>
2026-02-16 01:24:18 +0100

    clean

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,14 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
+
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <main class="page"> 
+        <h1>Home</h1>
+    </main>
 </template>
+<style>
+.page{
+    padding: 20px;
+}
+</style>
